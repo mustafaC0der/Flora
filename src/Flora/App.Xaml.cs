6ae0ebb1c506b0ce4597d097ba61b8570c17b969
mustafa <adhdstudios42@gmail.com>
2024-12-25 13:13:26 +0500

    Delete src/Flora directory

--- src/Flora/App.Xaml.cs
+++
@@ -1,8 +0,0 @@
-using System.Windows;
-
-namespace Flora
-{
-    public partial class App : Application
-    {
-    }
-}
--- src/Flora/Models/Note.cs
+++
@@ -1,9 +0,0 @@
-namespace Flora.Models
-{
-    public class Note
-    {
-        public string Title { get; set; }
-        public string Content { get; set; }  // Still using plain text, can be enhanced for rich text
-        public List<string> Tags { get; set; } = new List<string>();
-    }
-}
